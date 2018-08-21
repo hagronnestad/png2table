01581a43916591d6dc2aa4968853f2e31791714f
Hein Andre Grønnestad <hag@haugnett.no>
2018-08-21 22:03:25 +0200

    Console application implemented

---
+++ png2table/Png2Table.cs
@@ -0,0 +1,103 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.Text;
+
+namespace png2table
+{
+    internal class Png2Table
+    {
+        public string Convert(string imageFile, bool useColSpan = true, bool useStyleTag = false, bool antiAlias = true, string backgroundColor = "white")
+        {
+            var image = Image.FromFile(imageFile);
+
+            Bitmap b = null;
+
+            if (antiAlias)
+            {
+                b = new Bitmap(image.Width, image.Height, PixelFormat.Format32bppArgb);
+                var g = Graphics.FromImage(b);
+                g.Clear(ColorTranslator.FromHtml(backgroundColor));
+                g.DrawImage(image, 0, 0, image.Width, image.Height);
+
+            }
+            else
+            {
+                b = new Bitmap(image);
+            }
+
+            var sb = new StringBuilder();
+
+            if (useStyleTag)
+            {
+                sb.AppendLine("<style>");
+                sb.AppendLine(".png2table td { width: 1px; height: 1px; }");
+                sb.AppendLine("</style>");
+            }
+
+            sb.AppendLine($"<table{(useStyleTag ? " class=\"png2table\"" : "")} cellspacing=\"0\" cellpadding=\"0\">");
+
+            if (useColSpan)
+            {
+
+                for (int y = 0; y < image.Height; y++)
+                {
+                    sb.AppendLine("<tr>");
+
+                    var sameColorCount = 1;
+                    for (int x = 0; x < image.Width; x++)
+                    {
+                        var thisC = b.GetPixel(x, y);
+                        var nextC = x < image.Width - 1 ? b.GetPixel(x + 1, y) : Color.Empty;
+
+                        if (nextC == thisC)
+                        {
+                            sameColorCount++;
+                            continue;
+                        }
+
+                        if (thisC != nextC)
+                        {
+                            AddTdPixels(sb, !useStyleTag, sameColorCount, thisC);
+                            sameColorCount = 1;
+                        }
+                    }
+
+                    sb.AppendLine("</tr>");
+                }
+
+            }
+            else
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    sb.AppendLine("<tr>");
+
+                    for (int x = 0; x < image.Width; x++)
+                    {
+                        AddTdPixels(sb, !useStyleTag, 1, b.GetPixel(x, y));
+                    }
+
+                    sb.AppendLine("</tr>");
+                }
+            }
+
+            sb.AppendLine("</table>");
+            return sb.ToString();
+        }
+
+        private void AddTdPixels(StringBuilder sb, bool useSizingAttributes, int numberOfPixels, Color c)
+        {
+            var bgColorString = c.A == 0 ? "" : $" bgcolor=\"{ColorTranslator.ToHtml(c)}\"";
+            var sizingString = useSizingAttributes ? $" width=\"{numberOfPixels}\" height=\"1\"" : "";
+
+            if (numberOfPixels > 1)
+            {
+                sb.AppendLine($"<td{sizingString} colspan=\"{numberOfPixels}\"{bgColorString}></td>");
+            }
+            else
+            {
+                sb.AppendLine($"<td{sizingString}{bgColorString}></td>");
+            }
+        }
+    }
+}
--- png2table/Program.cs
+++ png2table/Program.cs
@@ -1,15 +1,104 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
+using System.IO;
 
 namespace png2table
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            if (args.Length == 0) PrintHelp();
+
+            var inputFile = string.Empty;
+            var outputFile = string.Empty;
+
+            var noColSpan = false;
+            var noAa = false;
+            var st = false;
+            var bgColor = Color.White.Name;
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                switch (args[i].Trim().ToLower())
+                {
+                    case "-nocolspan":
+                        noColSpan = true;
+                        break;
+
+                    case "-noaa":
+                        noAa = true;
+                        break;
+
+                    case "-st":
+                        st = true;
+                        break;
+
+                    case "-bg":
+                        if (args.Length >= i + 1)
+                        {
+                            bgColor = args[i + 1];
+                            i++;
+                        }
+                        break;
+
+                    default:
+                        if (inputFile == string.Empty)
+                        {
+                            inputFile = args[i];
+                        }
+                        else
+                        {
+                            outputFile = args[i];
+                        }
+                        break;
+                }
+            }
+
+            Console.WriteLine($"Converting {Path.GetFileName(inputFile)} to {Path.GetFileName(outputFile)}...");
+            Console.WriteLine("");
+            Console.WriteLine("Options:");
+            Console.WriteLine($"Use colspan attribue: {(noColSpan ? "no" : "yes")}");
+            Console.WriteLine($"Use antialiasing: {(noAa ? "no" : "yes")}");
+            if (noAa) Console.WriteLine($"Background color: {bgColor}");
+            Console.WriteLine($"Use style tag: {(st ? "yes" : "no")}");
+            Console.WriteLine("");
+
+            try
+            {
+                var p2t = new Png2Table();
+                var html = p2t.Convert(inputFile, !noColSpan, st, !noAa, bgColor);
+                File.WriteAllText(outputFile, html);
+
+                Console.WriteLine($"Input file size: {(new FileInfo(inputFile).Length / 1000)} kb");
+                Console.WriteLine($"Output file size: {(new FileInfo(outputFile).Length / 1000)} kb");
+                Console.WriteLine("");
+
+                Console.WriteLine("Conversion completed!");
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Exception:\n{ex}\n");
+                Console.WriteLine("Conversion failed!");
+            }
+            finally
+            {
+                Console.ResetColor();
+            }
+         
+            Console.ReadLine();
+        }
+
+        public static void PrintHelp()
+        {
+            Console.WriteLine("Usage: png2table [-nocolspan] [-st] [-noaa] [-bg html_color] image_file html_output_file");
+            Console.WriteLine("");
+            Console.WriteLine("Options:");
+            Console.WriteLine("\t -nocolspan \t\t Disables use of the colspan attribute. May increase file size substantially depending on input image.");
+            Console.WriteLine("\t -noaa \t\t\t Disables antialiasing, allows for transparent pixels.");
+            Console.WriteLine("\t -bg html_color \t Background color to use when using antialiasing. Default: white.");
+            Console.WriteLine("\t -st \t\t\t Outputs a style tag for additional styling. May improve compatibility in some browsers.");
         }
     }
 }
